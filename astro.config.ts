d5f2d3ef37d500df185b50ad4a81bb58bfe682bc
linchien26 <linchien@foxmail.com>
2026-03-04 05:56:01 +0800

    update astro.config.ts and src/config.ts for site

--- astro.config.ts
+++ astro.config.ts
@@ -26,8 +26,8 @@ const imageConfig = imageHostURL
   : {}
 
 export default defineConfig({
-  site,
-  base,
+  site: 'https://blog.liin.top',
+  base: '/',
   trailingSlash: 'always', // Not recommended to change
   prefetch: {
     prefetchAll: true,
